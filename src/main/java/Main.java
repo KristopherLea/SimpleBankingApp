97d7fd03e0c14846f79fe00ae1604f8f17e77244
Kristopher Lea <kristopherlea187@gmail.com>
2020-11-17 16:51:16 -0600

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       BankAccount bankAccount1 = new BankAccount("Kris","1");
+       bankAccount1.showMenu();
+    }
+}
